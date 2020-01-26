ee5f8b2e4dfd14bbbdd5adc72247392b5696165d
Pick1a1username <20301273+Pick1a1username@users.noreply.github.com>
2020-01-26 12:40:56 +0900

    20200126_1

--- chapter5/exercise-4/src/index.ts
+++ chapter5/exercise-4/src/index.ts
@@ -2,6 +2,7 @@ class RequestBuilder {
     private data: object | null = null
     private method: 'get' | 'post' | null = null
     private url: string | null = null
+    private response: string | null = null
 
     setMethod(method: 'get' | 'post'): this {
         this.method = method
@@ -18,18 +19,19 @@ class RequestBuilder {
         return this
     }
 
-    send() {
-        // ...
+    send(): this {
+        this.response = 'responsed'
+        return this
+
     }
-
 }
-
-
-
 
 
 new RequestBuilder()
     .setURL('/users')
     .setMethod('get')
     .setData({firstName: 'Anna'})
+    .send()
+
+new RequestBuilder()
     .send()
